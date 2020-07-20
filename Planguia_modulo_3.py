e9ca2e295c59bb0051af5f012de28f8e7d561b9b
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-07-19 22:48:58 -0300

    Add files via upload

--- Planguia_modulo_3.py
+++ Planguia_modulo_3.py
@@ -11,6 +11,16 @@ class EnviarEmail:
         self.revisao = revisao
 
     def preparar_email(self):
+        aba_porte = self.arquivo.get_sheet_by_name('Porte')
+        self.arquivo.remove_sheet(aba_porte)
+        aba_adc = self.arquivo.get_sheet_by_name('ADC')
+        self.arquivo.remove_sheet(aba_adc)
+        aba_previa = self.arquivo.get_sheet_by_name('Previa')
+        self.arquivo.remove_sheet(aba_previa)
+        aba_info_pblog = self.arquivo.get_sheet_by_name('Info Contrato - Pblog')
+        self.arquivo.remove_sheet(aba_info_pblog)
+        aba_chaves = self.arquivo.get_sheet_by_name('Chaves')
+        self.arquivo.remove_sheet(aba_chaves)
         dados_rev = self.aba.cell(row=1, column=7).value
         mes_medicao = self.aba.cell(row=1, column=2).value
         self.aba.cell(row=1, column=16).value = ''
@@ -35,31 +45,36 @@ class EnviarEmail:
         return sentenca
 
     def listar_destinatario(self):
-        aba1 = self.arquivo['Chaves']
-        contar_dest = aba1.max_row
+        t = Planguia_funcoes.openr('Projeto_planilha_Guia_Medição.xlsx', 'Chaves')
+        contar_dest = t[1].max_row
         lista_destinatario = []
         lista_destinatario1 = []
         lista_resultado = []
         for n in range(2, contar_dest + 1):
-            status = aba1.cell(row=n, column=6).value
+            status = t[1].cell(row=n, column=6).value
             if status == 'Sim':
-                endereco = aba1.cell(row=n, column=1).value
+                endereco = t[1].cell(row=n, column=1).value
                 lista_destinatario.append(endereco)
             elif status == 'Copy':
-                endereco2 = aba1.cell(row=n, column=1).value
+                endereco2 = t[1].cell(row=n, column=1).value
                 lista_destinatario1.append(endereco2)
         saudacao = Planguia_funcoes.saudar()
         destinatarios = '; '.join(lista_destinatario)
         destinatarios2 = '; '.join(lista_destinatario1)
         lista_resultado.append(destinatarios)
         corpo_email = saudacao + '\n\nSegue anexo a planilha guia de medição (Revisão ' + str(
-            self.revisao) + ').\nQualquer informação a acrescentar em alguma embarcação da frota, que não constar na planilha' \
-                            ', favor avisar para ajuste.\n\nATENÇÃO: PARA EMBARCAÇÕES QUE NÃO FORAM LIBERADAS ' \
-                            'PARA MEDIÇÃO VER NA COLUNA (N - "Embarcação Liberada") DA ABA (MEDIÇÃO).TODAS AS ALTERAÇÕES REALIZADAS ' \
-                            'NA PLANILHA GUIA SÃO REGISTRADAS NA ABA "Revisão".\n\nAs embarcações "Não Liberadas" estão sendo analisadas ' \
-                            'pela equipe de coordenação e controle das informações referente a frota sob a gestão do CMAR, e em breve esses ' \
-                            'equipamentos poderão sofrer alterações quanto ao seu status. Caso isso ocorra, uma nova r' \
-                            'evisão será emitida com a finalidade de ajustar a planilha guia.'
+            self.revisao) + ').\nQualquer informação a acrescentar em alguma embarcação da frota, ' \
+                            'que não constar na planilha, favor avisar para ajuste.' \
+                            '\n\nATENÇÃO: PARA EMBARCAÇÕES QUE NÃO FORAM LIBERADAS ' \
+                            'PARA MEDIÇÃO VER NA COLUNA (N - "Embarcação Liberada") DA ABA (MEDIÇÃO).' \
+                            'TODAS AS ALTERAÇÕES REALIZADAS ' \
+                            'NA PLANILHA GUIA SÃO REGISTRADAS NA ABA "Revisão".' \
+                            '\n\nAs embarcações "Não Liberadas" estão sendo analisadas ' \
+                            'pela equipe de coordenação e controle das informações ' \
+                            'referente a frota sob a gestão do CMAR, e em breve essas ' \
+                            'embarcações poderão sofrer alteração quanto ao seu status. ' \
+                            'Caso isso ocorra, uma nova revisão será emitida ' \
+                            'com a finalidade de ajustar a planilha guia.'
         lista_resultado.append(corpo_email)
         lista_resultado.append(destinatarios2)
         return lista_resultado
@@ -67,26 +82,25 @@ class EnviarEmail:
     def encaminhar_email(self):
         try:
             anexo = r'C:/Users/ay4m/Desktop/Python/projetos/' + self.preparar_email()
-            lista_dados = self.listar_destinatario()
+            lista_destinatario = self.listar_destinatario()
             o = win32com.client.Dispatch("Outlook.Application")
-            # lista_dados = contar.listar_destinatario_Petro(caminho, aba, rev)
             msg = o.CreateItem(0)
-            msg.To = lista_dados[0]
-            msg.CC = lista_dados[2]
+            msg.To = lista_destinatario[0]
+            msg.CC = lista_destinatario[2]
             msg.BCC = ''
             msg.Subject = 'CMAR - Planilha Guia de Medição - ' + self.mes[0] + ' - REVISÃO ' + str(self.revisao)
-            msg.Body = lista_dados[1] + '\nHistórico de ajustes realizados (Revisão ' + str(
-                self.revisao) + '):\n' + self.listar_revisao() + '\nAtenciosamente,' \
+            msg.Body = lista_destinatario[1] + '\nHistórico de ajustes realizados - Revisão ' + str(
+                self.revisao) + ':\n' + self.listar_revisao() + '\nAtenciosamente,' \
                                                                 '\nEquipe de Gerenciamento Marítimo\nLOEP/LOFF/GCI/CMAR\n' + self.mes[1]
             msg.Attachments.Add(anexo)
             msg.Send()
-            return '\nPrezado usuário, Email enviado com sucesso!'
+            return '\nEmail enviado com sucesso!'
         except:
-            return 'Erro ao enviar email'
+            return 'Erro ao enviar email!!!'
 
 
 def iniciar_3():
-    print('Preparando email para envio....................')
+    print('\nPreparando email para envio....................')
     t = Planguia_funcoes.openr('Projeto_planilha_Guia_Medição.xlsx', 'Medição')
     numero_mes = t[1].cell(row=1, column=16).value
     mes = Planguia_funcoes.definir_mes(numero_mes)
