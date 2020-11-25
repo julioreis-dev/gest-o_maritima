88288dd7b15ab102c3afdc958d2b3cc5189a4237
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-11-24 23:37:40 -0300

    Add files via upload

--- draft/App.py
+++ draft/App.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import modulo_planilha as pl
 import modulo_calculo as mc
+import modulo_email as em
 import time
-from tkinter import messagebox  # filedialog
 from time import sleep
 
 
@@ -38,20 +38,15 @@ def contract(pfile):
     return dictdados1
 
 
-def aplicationtime(function):
+def aplicationtime(func):
     def resultime(*args, **kwargs):
         t0 = time.time()
-        function(*args, **kwargs)
+        func(*args, **kwargs)
         t1 = time.time()
         temp = t1 - t0
         print('Tempo de execução : {} sec.'.format(round(temp, 2)))
 
     return resultime
-
-
-def message():
-    return messagebox.askyesno(title='Gerência de Contratos Marítimos - CMAR',
-                               message='Prezado usuário gostaria de emitir a planilha guia?')
 
 
 @aplicationtime
@@ -68,48 +63,72 @@ def opt1(origin, destination):
     plan.alocatedataship()
     plan.formatation()
 
+@aplicationtime
+def opt2(origin, destination):
+    x = origin
+    y = destination
+    print('Em construção!!!!')
 
-def opt2(orig, dest):
+@aplicationtime
+def opt3(orig, dest):
     calculate = mc.CalcPlanGui(orig, dest)
     calculate.calcdata()
     print('Planilha guia finalizada com sucesso!!!')
+
+@aplicationtime
+def opt4(orig, dest):
+    corr = em.email(orig, dest)
+    revdata = corr.planinformation()
+    corr.planformat(revdata)
+    y = corr.filesname(revdata)
+    tocc = corr.preparedest()
+    corr.planformat(revdata)
+    issue = corr.emailcontent(revdata)
+    statusend = corr.sendemail(revdata, tocc, issue)
+    print(statusend)
+
+def optgeneral(choose):
+    opt = {
+        1: opt1,
+        2: opt2,
+        3: opt3,
+        4: opt4,
+    }
+    operationchoosen = opt[choose]
+    return operationchoosen
 
 
 def main():
     try:
         flag = True
-        origin = r'C:\Users\Julio\Desktop\Projeto_planilha_Guia_Medição.xlsx'
-        destination = r'C:\Users\Julio\Desktop\planguia_draft.xlsx'
+        origin = r'C:\Users\ay4m\Desktop\planguia\planilha_Guia_Medição.xlsx'
+        destination = r'C:\Users\ay4m\Desktop\planguia\memoria_calculo.xlsx'
         while flag:
             answer = input('################################################'
                            '\nTipos de opções disponíveis nesta aplicação:'
                            '\nDigite 1 --> Emitir prévia da planilha guia.'
                            '\nDigite 2 --> Computar indices de inoperâncias. (Em construção!!!)'
-                           '\nDigite 3 --> Preparar versão final da planilha guia.'
+                           '\nDigite 3 --> Recalcular planilha.'
                            '\nDigite 4 --> (Opcional) - Enviar planilha guia para os gerentes e fiscais de contrato.'
                            '\nDigite 0 --> Sair.'
                            '\n################################################'
                            '\nPrezado usuário, escolha uma opção?')
             if answer.isdigit():
                 answer = int(answer)
-                if answer == 1:
-                    opt1(origin, destination)
-                    opt2(origin, destination)
-                    sleep(7)
-                elif answer == 2:
-                    print('Aplicação em construção!!!!')
-                    # opt2(destination)
-                    sleep(7)
-                elif answer == 3:
-                    opt2(origin, destination)
-                    sleep(3)
-                elif answer == 4:
-                    lastdest = r''
+                if answer in range(1, 5):
+                    operation = optgeneral(answer)
+                    operation(origin, destination)
+                    time.sleep(3)
                 elif answer == 0:
                     print('Finalizando................')
+                    time.sleep(1)
                     flag = False
+                else:
+                    print('\nPrezado usuário, tente uma opção válida!!!\n')
+                    time.sleep(3)
             else:
                 print('\nPrezado usuário, tente novamente digitando um numero válido!!!\n')
+                time.sleep(3)
     except Exception as err:
         print(f'Erro:\nErro de aplicação {err}')
         exit()
