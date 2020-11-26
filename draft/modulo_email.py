bd35f4bef5651b70d3322c1aeb00f6ec802ace0e
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-11-25 23:14:26 -0300

    Add files via upload

--- draft/modulo_email.py
+++ draft/modulo_email.py
@@ -3,14 +3,18 @@ from openpyxl import load_workbook
 from modulo_calculo import CalcPlanGui
 import time
 
-class email(CalcPlanGui):
+
+class Email(CalcPlanGui):
     def __init__(self, pathorigin, pathdest):
         super().__init__(pathorigin, pathdest)
 
+
     def planinformation(self):
+        # self.clearcell()
         t = time.localtime()
         resposta = []
-        month = input('\nQual é o numero do mês da planilha guia de medição? (ex:1-Janeiro, 2-Fevereiro, 3-Março, ...)?')
+        month = input(
+            '\nQual é o numero do mês da planilha guia de medição? (ex:1-Janeiro, 2-Fevereiro, 3-Março, ...)?')
         review = input('Qual é a revisão da planilha guia de medição? (ex:1, 2, 3,...)?')
         if review.isdigit():
             review = int(review)
@@ -24,7 +28,7 @@ class email(CalcPlanGui):
             else:
                 period = '26/' + str(month - 1) + '/' + str(t[0]) + ' a 25/' + str(month) + '/' + str(t[0])
             meses_ano = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio',
-                     'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+                         'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
             mes = meses_ano[month - 1]
             frase1 = mes + ' de ' + str(t[0])
             resposta.append(frase1)
@@ -32,21 +36,32 @@ class email(CalcPlanGui):
                      str(t[0]) + ' as ' + str(t[3]) + ':' + str(t[4]) + ':' + str(t[5]) + ')'
             frase3 = period
             resposta.append(frase3)
-            resposta.append('0'+str(review))
+            resposta.append('0' + str(review))
             resposta.append(frase2)
         return resposta
 
-    def filesname(self, namesdata):
-        filename = namesdata[0] + '- Planilha Guia_R'+ namesdata[2]
+    @staticmethod
+    def filesname(namesdata):
+        filename = namesdata[0] + '- Planilha Guia_R' + namesdata[2] + '.xlsx'
         return filename
 
-    def preparedest(self):
+
+    def clearcell(self):
         wb = load_workbook(self.pfile1)
+        ws = wb['Medição']
+        contador = ws.max_row
+        for col in range(1, 22):
+            for linha in range(3, contador+1):
+                ws.cell(row=linha, column=col).value = ''
+        wb.save(self.pfile1)
+
+    def preparedest(self):
+        wb = load_workbook(self.pfile3)
         ws = wb['Chaves']
         destiny = []
         copy = []
         cont = ws.max_row
-        for w in range(2, cont+1):
+        for w in range(2, cont + 1):
             status = ws.cell(row=w, column=6).value
             if status == 'Sim':
                 destiny.append(ws.cell(row=w, column=1).value)
@@ -58,19 +73,17 @@ class email(CalcPlanGui):
         finalcopy = '; '.join(copy)
         return finaldestiny, finalcopy
 
-
     def planformat(self, listdata):
-        wb = load_workbook(self.pfile1)
+        wb = load_workbook(self.pfile3)
         ws = wb['Medição']
         fields = [2, 5, 8]
         for i, n in enumerate(fields):
             ws.cell(row=1, column=n).value = listdata[i]
         wb.save(self.pfile1)
 
-
-    def sendemail(self, rev, dest, content):
+    def sendemail(self, rev, dest, content, anx):
         try:
-            anexo = self.pfile1
+            anexo = anx
             o = win32com.client.Dispatch("Outlook.Application")
             msg = o.CreateItem(0)
             msg.To = dest[0]
@@ -81,11 +94,10 @@ class email(CalcPlanGui):
             msg.Attachments.Add(anexo)
             index = anexo.rfind('/')
             extensao = anexo[index + 1:]
-            # lista_resposta = [extensao, '\nEmail enviado com sucesso!']
             msg.Send()
             return 'Email enviado com sucesso!'
-        except:
-            return 'Erro ao enviar email!!!'
+        except Exception as err:
+            return f'Erro: {err}\nDurante o processo de elaboração e envio do email um erro foi reportado!!!'
 
     @staticmethod
     def saudar():
@@ -101,30 +113,29 @@ class email(CalcPlanGui):
             c = 'Prezados Gerentes e Fiscais de contrato, Boa tarde!'
             return c
 
-
     def emailcontent(self, rev):
-        corpo_email = self.saudar() + '\n\nSegue anexo a planilha guia de medição (Revisão ' + str(rev[2]) + ').\nQualquer ' \
-                            'informação a acrescentar em alguma embarcação da frota, ' \
-                            'que não constar na planilha, favor avisar para ajuste.' \
-                            '\n\nATENÇÃO: PARA EMBARCAÇÕES QUE NÃO FORAM LIBERADAS ' \
-                            'PARA MEDIÇÃO VER NA COLUNA (N - "Embarcação Liberada") DA ABA "MEDIÇÃO".' \
-                            ' TODAS AS ALTERAÇÕES REALIZADAS ' \
-                            'NA PLANILHA GUIA SÃO REGISTRADAS NA ABA "REVISÃO".' \
-                            '\n\nAs embarcações "Não Liberadas" estão sendo analisadas ' \
-                            'pela equipe de coordenação e controle das informações ' \
-                            'referente a frota sob a gestão do CMAR, e em breve essas ' \
-                            'embarcações poderão sofrer alteração quanto ao seu status. ' \
-                            'Caso isso ocorra, uma nova revisão será emitida ' \
-                            'com a finalidade de ajustar a planilha guia.'
+        corpo_email = self.saudar() + '\n\nSegue anexo a planilha guia de medição (Revisão ' + str(
+            rev[2]) + ').\nQualquer ' \
+                      'informação a acrescentar em alguma embarcação da frota, ' \
+                      'que não constar na planilha, favor avisar para ajuste.' \
+                      '\n\nATENÇÃO: PARA EMBARCAÇÕES QUE NÃO FORAM LIBERADAS ' \
+                      'PARA MEDIÇÃO VER NA COLUNA (N - "Embarcação Liberada") DA ABA "MEDIÇÃO".' \
+                      ' TODAS AS ALTERAÇÕES REALIZADAS ' \
+                      'NA PLANILHA GUIA SÃO REGISTRADAS NA ABA "REVISÃO".' \
+                      '\n\nAs embarcações "Não Liberadas" estão sendo analisadas ' \
+                      'pela equipe de coordenação e controle das informações ' \
+                      'referente a frota sob a gestão do CMAR, e em breve essas ' \
+                      'embarcações poderão sofrer alteração quanto ao seu status. ' \
+                      'Caso isso ocorra, uma nova revisão será emitida ' \
+                      'com a finalidade de ajustar a planilha guia.'
 
-        subject = 'CMAR - Planilha Guia de Medição - ' + rev[0] + ' - REVISÃO ' + rev[2]
+        subject = '(TESTE) - CMAR - Planilha Guia de Medição - ' + rev[0] + ' - REVISÃO ' + rev[2]
         body = '\nHistórico de ajustes realizados - Revisão ' + rev[2] + ':\n' \
                + self.listar_revisao() + '\nAtenciosamente,\nEquipe de Gerenciamento Marítimo\nLOEP/LOFF/GCI/CMAR\n'
         return corpo_email, subject, body
 
-
     def listar_revisao(self):
-        wb = load_workbook(self.pfile1)
+        wb = load_workbook(self.pfile3)
         ws = wb['Revisão']
         lista = []
         numero_linhas = ws.max_row
