e2df01a8fe4a41864f47264d05a87755d5fa2cc1
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-12-07 15:44:00 -0300

    Update mergedatas.py

--- report/mergedatas.py
+++ report/mergedatas.py
@@ -6,7 +6,7 @@ def merge():
     outputfile = r'C:\Users\Julio\Desktop\teste\Relatório_CMAR.xlsx'
     df3 = pd.merge(df1, df2, on='Embarcação', how='left')
     df_final = df3[['Equipamento', 'Embarcação', 'Classe', 'Porte', 'Regional', 'Regional CMAR', 'Dias Medir',
-                    'Indisp', 'Medir', 'Centro de Custo', 'Valor Petrobras $', 'Valor PBLOG $', 'Total $']]
+                    'Indisp', 'Medir', 'Centro de Custo', 'Valor Petro $', 'Valor PBL $', 'Total $']]
     df_final = df_final.drop_duplicates('Equipamento', keep='first')
     # df_final['Taxa Diária'] = (df_final['Total $'] / df_final['Medir']).round(2)
     df_final.to_excel(outputfile, index=False)
