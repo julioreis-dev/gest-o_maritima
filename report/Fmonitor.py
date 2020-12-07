51b9249b3d01503b7dbc91cd224cf069d5231bf4
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-12-07 15:48:26 -0300

    Update Fmonitor.py

--- report/Fmonitor.py
+++ report/Fmonitor.py
@@ -11,12 +11,12 @@ class PlanMonitor:
     def formatacao(self):
         df_new = self.df.fillna(0)
         listcolumns = ['EMBARCAÇÃO\nPARCELA BRL\n(COM REAJUSTE)', 'EMBARCAÇÃO\nPARCELA BRL\n(COM REAJUSTE).1']
-        df_new['Valor Petrobras $'] = df_new[listcolumns[0]] / self.dolar
-        df_new['Valor Petrobras $'] = df_new[['EMBARCAÇÃO\nPARCELA USD', 'Valor Petrobras $']].sum(axis=1).round(2)
-        df_new['Valor PBLOG $'] = df_new[listcolumns[1]] / self.dolar
-        df_new['Valor PBLOG $'] = df_new[['EMBARCAÇÃO PARCELA USD', 'Valor PBLOG $']].sum(axis=1).round(2)
-        df_new['Total $'] = df_new[['Valor Petrobras $', 'Valor PBLOG $']].sum(axis=1).round(2)
-        df_new = df_new[['Embarcação', 'Valor Petrobras $', 'Valor PBLOG $', 'Total $']]
+        df_new['Valor Petro $'] = df_new[listcolumns[0]] / self.dolar
+        df_new['Valor Petro $'] = df_new[['EMBARCAÇÃO\nPARCELA USD', 'Valor Petro $']].sum(axis=1).round(2)
+        df_new['Valor PBL $'] = df_new[listcolumns[1]] / self.dolar
+        df_new['Valor PBL $'] = df_new[['EMBARCAÇÃO PARCELA USD', 'Valor PBL $']].sum(axis=1).round(2)
+        df_new['Total $'] = df_new[['Valor Petrobras $', 'Valor PBL $']].sum(axis=1).round(2)
+        df_new = df_new[['Embarcação', 'Valor Petro $', 'Valor PBL $', 'Total $']]
         df = df_new.copy()
         df['Embarcação'] = df_new.Embarcação.str.upper()
         df.to_excel(r'C:\Users\Julio\Desktop\teste\analise_monitoramento.xlsx', index=False)
