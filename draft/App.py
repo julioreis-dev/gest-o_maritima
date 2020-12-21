0df47f1d6637447e4154c46e4c41f3d979a4295e
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-12-21 19:47:24 -0300

    Add files via upload

--- draft/App.py
+++ draft/App.py
@@ -1,3 +1,4 @@
+from tkinter import *
 from tkinter import messagebox
 import pandas as pd
 import modulo_planilha as pl
@@ -5,10 +6,10 @@ import modulo_calculo as mc
 import modulo_email as em
 import modulo_backupfiles as mb
 import time
-from tkinter import *
 
-origin = r'C:\Users\Julio\Desktop\teste\Projeto_planilha_Guia_Medição.xlsx'
-destination = r'C:\Users\Julio\Desktop\teste\planguia_draft.xlsx'
+
+origin = r'C:\Users\ay4m\Desktop\planguia\planilha_Guia_Medição.xlsx'
+destination = r'C:\Users\ay4m\Desktop\planguia\planguia_draft.xlsx'
 
 
 def prl(pfile):
@@ -26,8 +27,10 @@ def porte(pfile):
     df1 = df1.to_dict('list')
     lisvalor = df1['Classe Desmembrada']
     for valor in lisvalor:
-        for pt in ('%s' % (df1['PORTE'][i]) for i, v in enumerate(lisvalor) if v == valor):
-            dictdados[valor] = pt
+        pt = list(('%s' % (df1['PORTE'][i]) for i, v in enumerate(lisvalor) if v == valor))
+        dictdados[valor] = pt[0]
+        # for pt in ('%s' % (df1['PORTE'][i]) for i, v in enumerate(lisvalor) if v == valor):
+        #     dictdados[valor] = pt
     return dictdados
 
 
@@ -45,11 +48,14 @@ def contract(pfile):
 
 def aplicationtime(func):
     def resultime(*args, **kwargs):
+        clean()
         t0 = time.time()
         func(*args, **kwargs)
         t1 = time.time()
         temp = t1 - t0
-        print('Tempo de execução : {} sec.'.format(round(temp, 2)))
+        total_time = 'Tempo de execução : {} sec.'.format(round(temp, 2))
+        canvas1.itemconfig(result_time, text=total_time)
+        window.after(5000, func=clean)
     return resultime
 
 
@@ -68,72 +74,80 @@ def opt1(orig, desti):
         plan.alocatedataship()
         plan.formatation()
         opt3(orig, desti)
-        msg = 'Prévia da planilha realizada com sucesso!!!'
-        cav.itemconfig(result, text=msg)
+        msg = 'Prévia da planilha guia realizada com sucesso!'
+        canvas0.itemconfig(result, text=msg)
     except Exception as err:
         handleerror(err)
 
 
 @aplicationtime
-def opt2(origin, destination):
-    x = origin
-    y = destination
-    print('Em construção!!!!')
+def opt2():
+    # def opt2(origin, destination):
+    # x = origin
+    # y = destination
+    mesgopt2 = 'Em construção!!!!'
+    canvas0.itemconfig(result, text=mesgopt2)
+    window.after(5000, func=clean)
 
 
 @aplicationtime
 def opt3(orig, dest):
-    # window.after(0000, func=clean)
     calculate = mc.CalcPlanGui(orig, dest)
     calculate.calcdata()
-    retorno = 'Calculo de rateio aplicado.\nPlanilha guia finalizada com sucesso!!!'
-    cav.itemconfig(result, text=retorno)
+    calculate.sheetconfiguration()
+    calculate.finalversion()
+    calculate.sendproduct()
+    retorno = 'Calculo da frota realizada com sucesso.\nPlanilha guia finalizada, pronta para o envio!!!'
+    canvas0.itemconfig(result, text=retorno)
     window.after(5000, func=clean)
 
 
 @aplicationtime
 def opt4(orig, dest):
-    corr = em.Email(orig, dest)
-    revdata = corr.planinformation()
-    corr.planformat(revdata)
-    namefiley = corr.filesname(revdata)
-    tocc = corr.preparedest()
-    corr.planformat(revdata)
-    issue = corr.emailcontent(revdata)
+    version_number = optversion()
+    month_number = optmonth()
+    mail = em.Email(orig, dest, month_number, version_number)
+    revdata = mail.planinformation()
+    mail.planformat(revdata)
+    namefiley = mail.filesname(revdata)
+    tocc = mail.preparedest()
+    mail.planformat(revdata)
+    issue = mail.emailcontent(revdata)
     back = mb.Backup(orig, dest, namefiley)
     back.verificar_pasta()
     nameadress = back.mover_arquivo()
     back.preparar_email(nameadress)
-    statusend = corr.sendemail(revdata, tocc, issue, nameadress)
-    print(statusend)
-
-
-def optgeneral(choose):
-    opt_general = {
-        1: opt1,
-        2: opt2,
-        3: opt3,
-        4: opt4,
-    }
-    operationchoosen = opt_general[choose]
-    return operationchoosen
+    statusend = mail.sendemail(revdata, tocc, issue, nameadress)
+    canvas0.itemconfig(result, text=statusend)
+    window.after(5000, func=clean)
 
 
 def clean():
-    cav.itemconfig(result, text='')
+    canvas0.itemconfig(result, text='')
+    canvas1.itemconfig(result, text='')
+
+
+def optmonth():
+    return opt_radio.get()
+
+
+def optversion():
+    return version.get()
 
 
 def handleerror(erro):
-    messagebox.showerror(title='Tratamento de erro', message=f'Erro:\nErro de aplicação {erro}')
+    messagebox.showerror(title='Mensagem de erro', message=f'Erro:\n{erro}')
 
 
+BLACK = '#000000'
+CINZA = '#B4B7BF'
 BLUE = '#04d8fb'
 GREEN = '#a8dda8'
 t = time.localtime()
 window = Tk()
-window.title(f'Contratos marítimos {t[0]} - CMAR')
-window.minsize(width=750, height=400)
-window.config(padx=5, pady=5, bg=GREEN)
+window.title(f'SISTEMA DE GERENCIAMENTO DE MEDIÇÃO {t[0]} - LOEP/LOFF/GCI/CMAR')
+window.minsize(width=730, height=350)
+window.config(padx=15, pady=15, bg=GREEN)
 window.iconbitmap('ship-icon-png-29.ico')
 
 canvas = Canvas(width=245, height=110, bg=GREEN, highlightthickness=0)
@@ -141,20 +155,24 @@ petro = PhotoImage(file='logo.png')
 canvas.create_image(125, 55, image=petro)
 canvas.grid(row=0, column=1, padx=5, pady=5)
 
-cav = Canvas(width=300, height=100, bg=GREEN, highlightthickness=0)
-result = cav.create_text(150, 20, text='', font=('Ariel', 10, 'bold'))
-cav.grid(row=7, column=1, padx=0, pady=0)
+canvas0 = Canvas(width=300, height=100, bg=GREEN, highlightthickness=0)
+result = canvas0.create_text(150, 20, text='', font=('Ariel', 10, 'bold'))
+canvas0.grid(row=7, column=1, padx=0, pady=0)
 
-# Label
-my_label = Label(text=f'SISTEMA DE GERENCIAMENTO DE CONTRATOS',
-                 fg='black', bg=GREEN, font=('Arial', 10, 'bold'))
-my_label.grid(row=1, column=1, padx=15, pady=15)
+canvas1 = Canvas(width=300, height=30, bg=GREEN, highlightthickness=0)
+result_time = canvas1.create_text(150, 20, text='', font=('Ariel', 10, 'bold'))
+canvas1.grid(row=8, column=1, padx=0, pady=0)
 
-version_label = Label(text='Versão',
+# Construção das Label da interface gráfica
+my_label = Label(text=f'CONTRATOS MARÍTIMOS - CMAR',
+                 fg='black', bg=GREEN, font=('Arial', 12, 'bold'))
+my_label.grid(row=1, column=1, padx=5, pady=5)
+
+version_label = Label(text='Revisão',
                       fg='black', bg=GREEN, font=('Arial', 12, 'bold'))
-version_label.grid(row=2, column=1, padx=5, pady=5)
+version_label.grid(row=2, column=1)
 
-med_label = Label(text='Escolha o mês:',
+med_label = Label(text='Mês de medição:',
                   fg='black', bg=GREEN, font=('Arial', 12, 'bold'))
 med_label.grid(row=2, column=2, padx=5, pady=5)
 
@@ -162,93 +180,61 @@ med_label = Label(text='Aplicações disponíveis:',
                   fg='black', bg=GREEN, font=('Arial', 12, 'bold'))
 med_label.grid(row=2, column=0, padx=5, pady=5)
 
-# botão
-previa_buttom = Button(text='1.Emitir prévia', font=('Arial', 12, 'bold'), width=12, height=1)
+# Construção dos botões da interface gráfica
+previa_buttom = Button(text='1. Emitir prévia', font=('Arial', 12, 'bold'),
+                       width=12, height=1, anchor=NW, activebackground=CINZA, bd=5)
 previa_buttom['command'] = lambda: opt1(origin, destination)
 previa_buttom.grid(row=3, column=0)
 
-inoper_buttom = Button(text='2.Indisp', font=('Arial', 12, 'bold'), width=12, height=1)
+inoper_buttom = Button(text='2. Inoperância', font=('Arial', 12, 'bold'),
+                       width=12, height=1, anchor=NW, activebackground=CINZA, bd=5)
+inoper_buttom['command'] = lambda: opt2()
 inoper_buttom.grid(row=4, column=0, padx=5, pady=5)
 
-calcular_buttom = Button(text='3.Versão final', font=('Arial', 12, 'bold'), width=12, height=1)
+calcular_buttom = Button(text='3. Calcular', font=('Arial', 12, 'bold'),
+                         width=12, height=1, anchor=NW, activebackground=CINZA, bd=5)
 calcular_buttom['command'] = lambda: opt3(origin, destination)
 calcular_buttom.grid(row=5, column=0)
 
-email_buttom = Button(text='4.Enviar email', font=('Arial', 12, 'bold'), width=12, height=1)
+email_buttom = Button(text='4. Enviar email', font=('Arial', 12, 'bold'),
+                      width=12, height=1, anchor=NW, activebackground=CINZA, bd=5)
 email_buttom['command'] = lambda: opt4(origin, destination)
 email_buttom.grid(row=6, column=0, padx=5, pady=5)
 
-opt = IntVar()
-opt.set(t[1])
+opt_radio = IntVar()
+opt_radio.set(t[1])
 month = [("Janeiro", 1), ("Fevereiro", 2), ("Março", 3), ("Abril", 4), ("Maio", 5), ('Junho', 6)]
 month2 = [('Julho', 7), ('Agosto', 8), ('Setembro', 9), ('Outubro', 10), ('Novembro', 11), ('Dezembro', 12)]
+
+# Construção dos radio buttom da interface gráfica
 incr = 0
 for mes, val in month:
     Radiobutton(window,
                 text=mes,
                 padx=20,
-                variable=opt,
+                variable=opt_radio,
                 bg=GREEN,
                 font=('Arial', 10, 'bold'),
-                # command=ShowChoice,
-                value=val).place(x=500, y=210 + incr)
+                command=optmonth,
+                value=val).place(x=450, y=190 + incr)
     incr += 25
+
 incr2 = 0
 for mes2, val2 in month2:
     Radiobutton(window,
                 text=mes2,
                 padx=20,
-                variable=opt,
+                variable=opt_radio,
                 bg=GREEN,
                 font=('Arial', 10, 'bold'),
-                # command=ShowChoice,
-                value=val2).place(x=610, y=210 + incr2)
+                command=optmonth,
+                value=val2).place(x=570, y=190 + incr2)
     incr2 += 25
-version = Spinbox(window, from_=0, to=10, width=2, font=('Arial', 14, 'bold'), bg=GREEN)
+
+# Construção do spinbox da interface gráfica
+version = Spinbox(window, from_=0, to=9, width=2, font=('Arial', 14, 'bold'), bg=GREEN)
 version.grid(row=3, column=1)
 window.mainloop()
-
-# flag = True
-# origin = r'C:\Users\Julio\Desktop\Projeto_planilha_Guia_Medição.xlsx'
-# destination = r'C:\Users\Julio\Desktop\planguia_draft.xlsx'
-# while flag:
-#     answer = input('################################################'
-#                    '\nTipos de opções disponíveis nesta aplicação:'
-#                    '\nDigite 1 --> Emitir prévia da planilha guia.'
-#                    '\nDigite 2 --> Computar indices de inoperâncias. (Em construção!!!)'
-#                    '\nDigite 3 --> Preparar versão final da planilha guia.'
-#                    '\nDigite 4 --> (Opcional) - Enviar planilha guia para os gerentes e fiscais de contrato.'
-#                    '\nDigite 0 --> Sair.'
-#                    '\n################################################'
-#                    '\nPrezado usuário, escolha uma opção?')
-#     if answer.isdigit():
-#         answer = int(answer)
-#         if answer == 1:
-#             opt1(origin, destination)
-#             opt2(origin, destination)
-#             sleep(7)
-#         elif answer == 2:
-#             print('Aplicação em construção!!!!')
-#             # opt2(destination)
-#             sleep(7)
-#         elif answer == 3:
-#             opt2(origin, destination)
-#             sleep(3)
-#         elif answer == 4:
-#             lastdest = r''
-#         elif answer == 0:
-#             print('Finalizando................')
-#             flag = False
-#     else:
-#         print('\nPrezado usuário, tente novamente digitando um numero válido!!!\n')
-# except Exception as err:
-#     handleerror(err)
-# erro = f'Erro:\nErro de aplicação {err}'
-# cav = Canvas(width=300, height=100, bg='#a8dda8', highlightthickness=0)
-# result = cav.create_text(150, 20, text='', font=('Ariel', 10, 'bold'))
-# cav.grid(row=7, column=1, padx=0, pady=0)
-# cav.itemconfig(result, text=erro)
-
 
 # if __name__ == '__main__':
 #     main()
